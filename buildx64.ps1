489fa782cc1eb9c66a4eca1ea2b260ab8775f786
霜序零一 <942708116@qq.com>
2022-12-25 13:00:20 +0800

    update_221225

---
+++ buildx64.ps1
@@ -0,0 +1,3 @@
+cd C:\Users\94270\Documents\GitHub\dta-mg-client-launcher
+
+dotnet publish --configuration Release --no-self-contained -p:PublishSingleFile=true -r win-x64 -p:DebugType=embedded -p:AssemblyVersion=2.0.2 -p:FileVersion=2.0.2 -p:InformationalVersion=2.0.2.Branch.master.by.霜序零一
