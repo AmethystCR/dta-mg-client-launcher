25b57d4b4424dfbe245878eb4b5ae2567f883279
霜序零一 <942708116@qq.com>
2023-01-30 10:56:35 +0800

    Create BuildRelease.ps1

---
+++ BuildRelease.ps1
@@ -0,0 +1,3 @@
+cd C:\Users\94270\Documents\GitHub\dta-mg-client-launcher
+
+dotnet publish --configuration Release -p:DebugType=embedded -p:AssemblyVersion=2.0.5 -p:FileVersion=2.0.5 -p:InformationalVersion=Internal_Release_V2.0.5 -p:Product=LDS_Client_Launcher
