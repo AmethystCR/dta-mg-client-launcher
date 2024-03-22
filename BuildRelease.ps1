960e6a15284c33046875cbabbcd245114eb78636
霜序零一 <942708116@qq.com>
2024-03-22 10:24:43 +0800

    custom

---
+++ BuildRelease.ps1
@@ -0,0 +1,5 @@
+cd C:\Users\94270\Documents\GitHub\dta-mg-client-launcher
+
+dotnet publish --framework net40 --configuration Release -p:DebugType=embedded -p:AssemblyVersion=2.0.7 -p:FileVersion=2.0.7 -p:InformationalVersion=Release_V2.0.7 -p:Product=LLSClientLauncher
+
+pause
