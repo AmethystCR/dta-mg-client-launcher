2409e4c7eeb584b8c6fbee9a87c6b52aceaf531a
霜序零一 <942708116@qq.com>
2024-09-12 17:29:58 +0800

    Update BuildRelease.ps1

--- BuildRelease.ps1
+++ BuildRelease.ps1
@@ -1,5 +1,5 @@
 cd C:\Users\94270\Documents\GitHub\dta-mg-client-launcher
 
-dotnet publish --framework net40 --configuration Release -p:DebugType=embedded -p:AssemblyVersion=2.0.7 -p:FileVersion=2.0.7 -p:InformationalVersion=Release_V2.0.7 -p:Product=LLSClientLauncher
+dotnet publish --framework net40 --configuration Release -p:DebugType=embedded -p:AssemblyVersion=2.0.8 -p:FileVersion=2.0.8 -p:InformationalVersion=Release_V2.0.8 -p:Product=LLSClientLauncher
 
 pause
